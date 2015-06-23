0f6fbe29372903221eedabd34a592aedd19fc04d
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-06-23 16:33:59 -0400

    Add mailer and job for mini-lecture

--- app/jobs/send_stats_job.rb
+++ app/jobs/send_stats_job.rb
@@ -5,21 +5,3 @@ class SendStatsJob < ActiveJob::Base
     Family.create!(species: species)
   end
 end
-
-# Add gems 'delayed_job_active_record' & 'daemons'
-# bundle install
-# In config/application.rb:
-  # config.active_job.queue_adapter = :delayed_job
-  # config.autoload_paths << Rails.root.join('app/jobs')
-# rails generate delayed_job:active_record
-# rake db:migrate
-# bin/delayed_job start
-# rails generate job JobName
-# Somewhere in our code: JobName.perform_later(params[:something_important])
-# ... when you are done coding:
-# bin/delayed_job stop
-
-# rails g mailer MailerNameMailer action_name other_action_name
-# Modify views and mailers as you see fit
-# Add gmail style config to environments/development.rb: http://guides.rubyonrails.org/action_mailer_basics.html#action-mailer-configuration-for-gmail
-# Somewhere in our code: MailerNameMailer.other_action_name.deliver_now
